f9b2ddd4f97a3f14897b0c673c5fe97e3a67809d
AlexHrabar <34456105+AlexHrabar@users.noreply.github.com>
2022-01-17 10:22:42 +0200

    Create compas.py

---
+++ compas.py
@@ -0,0 +1,6 @@
+def direction(facing, turn):
+    directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+    turns = turn // 45
+    start = directions.index(facing)
+    end = (start + turns) % len(directions)
+    return directions[end]
